80eda227a20bc08029666d9b62dd7542621dec59
Евг Ерм <ghjghj1086@gmail.com>
2019-06-23 23:57:09 +0400

    Change firebase setting

--- src/firebase.credentials.ts
+++ src/firebase.credentials.ts
@@ -5,5 +5,6 @@ export const FIREBASE_CONFIG = {
     projectId: 'myapplication-f5865',
     storageBucket: 'myapplication-f5865.appspot.com',
     messagingSenderId: '295866738626',
+    appId: '1:295866738626:web:aa3fcc9c1e0e4a3a',
     yandexMoney: '41001360993691'
 };
