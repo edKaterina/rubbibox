816eb3c0386541edfdbc40e6181b3dc6066dce95
Ganin Dmitry <ganin.dmitry63@gmail.com>
2019-06-24 15:15:19 +0400

    Переписал сервис проверки авторизации пользователя

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,91 +1,24 @@
-import { Injectable } from '@angular/core';
-import * as firebase from 'firebase/app';
-import { ReplaySubject, Observable, Subject } from 'rxjs';
-import { AngularFireDatabase } from '@angular/fire/database';
-import { HttpClient } from '@angular/common/http';
-import { CoreService } from './core.service';
-import { FIREBASE_CONFIG } from '../../firebase.credentials';
+import {Injectable} from '@angular/core';
+import {AngularFireAuth} from "@angular/fire/auth";
+import {first} from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthService {
 
-  public state: ReplaySubject<firebase.User> = new ReplaySubject(1);
-
-  constructor(
-    private db: AngularFireDatabase,
-    private httpClient: HttpClient,
-    private coreService: CoreService
-  ) {
-    firebase.auth().onAuthStateChanged(authData => {
-      if (authData != null) {
-        console.log('onAuthStateChanged', authData.uid);
-        this.state.next(authData);
-      } else {
-        firebase.auth().signInAnonymously();
-      }
-    });
-  }
-
-  // авторизация пользователя
-  // возвращает в промис логин пользователя
-  async auth() {
-    return new Promise((resolve, reject) => {
-      this.state.subscribe(authData => {
-        resolve(authData.uid);
-      });
-    });
-  }
-
-  // функция для guard обеспечивающая авторизацию
-  guardAuth(): Observable<any> {
-    const isAuth = new Subject();
-
-    this.auth().then(() => {
-      isAuth.next(true);
-      isAuth.complete();
-    });
-
-    return isAuth;
-  }
-
-  // Сохраненный логин авторизованного пользователя
-  getLogin(): string {
-    return firebase.auth().currentUser.uid;
-  }
-
-  // Присоединение номера телефона к аккаунту
-  linkAuthPhone(phone: string) {
-    if (!phone) {
-      this.coreService.presentToast('Введите номер телефона');
-      return;
+    constructor(private fAuth: AngularFireAuth) {
     }
 
-    if (phone.substr(0, 1) === '+') {
-      phone = phone.substr(1);
+    // Сохраненный логин авторизованного пользователя
+    async auth() {
+        const user = await this.fAuth.authState.pipe(first()).toPromise();
+        return user ? user.uid : null;
     }
 
-    const server = `https://us-central1-${FIREBASE_CONFIG.projectId}.cloudfunctions.net`;
-    const uid = firebase.auth().currentUser.uid;
+    // выход из учетной записи
+    logout() {
+        return this.fAuth.auth.signOut();
+    }
 
-    this.httpClient.get(`${server}/getAuthID?phone=${phone}&uid=${uid}`).subscribe(data => {
-      if (data['error']) {
-        this.coreService.presentToast(data['error']);
-        return;
-      }
-
-      document.location.href = data['call'];
-      this.db.object(`authHistory/${data['key']}/call_id`).valueChanges().subscribe(value => {
-        if (value) {
-          firebase.auth().signInWithCustomToken(value as string);
-        }
-      });
-    });
-  }
-
-  // выход из учетной записи
-  logout() {
-    firebase.auth().signOut();
-  }
 }
