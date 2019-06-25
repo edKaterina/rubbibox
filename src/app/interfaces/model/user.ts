c40e59998b790f8dcfd36da554ece7a18bafc3d1
Ganin Dmitry <ganin.dmitry63@gmail.com>
2019-06-25 12:15:41 +0400

    Отредактировал модель пользователя

--- src/app/interfaces/model/user.ts
+++ src/app/interfaces/model/user.ts
@@ -1,9 +1,19 @@
-export interface User {
+export class User {
     id?: string;
     data: {
-        fio: string,
-        img: Array<string>,
-        info: string,
+        fio?: string,
+        img?: Array<string>,
+        info?: string,
         phone: string,
     };
+
+    constructor(id = '', {fio = '', img = [], info = '', phone = ''}) {
+        this.id = id;
+        this.data = {
+            fio,
+            img,
+            info,
+            phone
+        }
+    }
 }
