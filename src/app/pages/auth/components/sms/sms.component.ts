c3a1794ed5983d9f70e70f118e8c1331ecea325c
Ganin Dmitry <ganin.dmitry63@gmail.com>
2019-06-25 12:00:20 +0400

    При логине пользователя записываю телефон в базу данных

--- src/app/pages/auth/components/sms/sms.component.ts
+++ src/app/pages/auth/components/sms/sms.component.ts
@@ -3,6 +3,8 @@ import {AlertController, NavController} from "@ionic/angular";
 
 import {AngularFireAuth} from '@angular/fire/auth';
 import * as firebase from 'firebase';
+import {User} from "../../../../interfaces/model/user";
+import {UserService} from "../../../../services/user/user.service";
 
 @Component({
     selector: 'sms',
@@ -18,7 +20,9 @@ export class SmsAuthComponent implements AfterViewInit {
         private fAuth: AngularFireAuth,
         private alertCtrl: AlertController,
         private navCtrl: NavController,
-    ) {}
+        private userSerivce: UserService
+    ) {
+    }
 
     ngAfterViewInit() {
         this.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {'size': 'invisible'});
@@ -37,13 +41,18 @@ export class SmsAuthComponent implements AfterViewInit {
     async applyCode(confirmationResult) {
         const prompt = await this.alertCtrl.create({
             header: 'Введите СМС-код',
-            inputs: [{name: 'confirmationCode', placeholder: 'XXXXXX',type: 'number', min: 0, max: 999999}],
+            inputs: [{name: 'confirmationCode', placeholder: 'XXXXXX', type: 'number', min: 0, max: 999999}],
             buttons: [
                 {
                     text: 'Отправить',
                     handler: (data) => {
-                        const credential = firebase.auth.PhoneAuthProvider.credential(confirmationResult.verificationId,data.confirmationCode);
-                        this.fAuth.auth.signInWithCredential(credential).then((result)=>{
+                        const credential = firebase.auth.PhoneAuthProvider.credential(confirmationResult.verificationId, data.confirmationCode);
+                        this.fAuth.auth.signInWithCredential(credential).then((result) => {
+                            const user: User = {
+                                id: result.uid,
+                                data: {phone: result.phoneNumber}
+                            };
+                            this.userSerivce.edit(user);
                             this.navCtrl.navigateRoot('/');
                         }).catch((error) => {
                             console.log(error);
