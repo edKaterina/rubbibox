4c77b96c1e15f88955f3a578b6b7a8e7ff8036b3
Ganin Dmitry <ganin.dmitry63@gmail.com>
2019-06-26 10:54:00 +0400

    Почистил лишнее в system таб

--- src/app/system/system.page.ts
+++ src/app/system/system.page.ts
@@ -1,6 +1,5 @@
 import {AuthService} from './../services/auth.service';
 import {Component} from '@angular/core';
-import {NotificationService} from '../services/notification.service';
 import {NavigationStart, Router} from "@angular/router";
 
 @Component({
@@ -12,17 +11,10 @@ export class SystemPage {
 
     public enable = true;
 
-    public countBadge: number;
-
     constructor(
         private authService: AuthService,
-        private notificationService: NotificationService,
         private router: Router
     ) {
-        //this.notificationService.initNotify();
-        this.notificationService.getBadge().subscribe(count => {
-            this.countBadge = count;
-        });
     }
 
     ngOnInit() {
