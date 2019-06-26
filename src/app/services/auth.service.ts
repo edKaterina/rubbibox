99e7f772c6d8b368d7dc33d555ffb3ca81afc30f
Ganin Dmitry <ganin.dmitry63@gmail.com>
2019-06-26 09:58:23 +0400

    Добавил в authservice асинхронное получение uid

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/auth";
+import {map} from "rxjs/operators";
 
 @Injectable({
     providedIn: 'root'
@@ -13,6 +14,12 @@ export class AuthService {
     getLogin() {
         const uid = this.fAuth.auth.currentUser.uid;
         return uid?uid:null;
+    }
+
+    // Сохраненный логин авторизованного пользователя(асинхронный)
+    async auth(){
+        const uid = await this.fAuth.user.pipe(map(user=>{return user.uid})).toPromise();
+        return await uid?uid:null;
     }
 
     // выход из учетной записи
