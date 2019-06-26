399ab225845b84416768519799e8114f4d1975ef
Ganin Dmitry <ganin.dmitry63@gmail.com>
2019-06-26 10:52:06 +0400

    Роуты табов

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {canActivate, redirectUnauthorizedTo} from "@angular/fire/auth-guard";
 
+const redirectUnauthorizedToLogin = redirectUnauthorizedTo(['/auth']);
 const routes: Routes = [
 
     {
@@ -11,7 +13,12 @@ const routes: Routes = [
 
     {path: 'system', loadChildren: './system/system.module#SystemModule'},
     {path: 'auth', loadChildren: './pages/auth/login/login.module#LoginPageModule'},
-    {path: 'chat/:id', loadChildren: '../../OtherPages/chat/chat.module#ChatPageModule'},
+    {
+        path: 'chat/:id', loadChildren: './pages/messenger/chat/chat.module#ChatPageModule',
+        ...canActivate(redirectUnauthorizedToLogin)
+    },
+
+
 ];
 
 @NgModule({
--- src/app/system/system.router.module.ts
+++ src/app/system/system.router.module.ts
@@ -1,23 +1,32 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SystemPage} from './system.page';
+import {canActivate, redirectUnauthorizedTo} from "@angular/fire/auth-guard";
+
+const redirectUnauthorizedToLogin = redirectUnauthorizedTo(['/auth']);
 
 const routes: Routes = [
-
-
     {
         path: '', component: SystemPage,
-
         children: [
-
-            {path: '', redirectTo: 'offers'},
-
-            {path: 'offers', loadChildren: '../pages/offer/offer.module#OfferModule'},
-
-            {path: 'dialogs', loadChildren: '../pages/dialog/dialog.module#DialogModule'},
-
-            {path: 'profile', loadChildren: '../pages/profile/profile.module#ProfileModule'}
-
+            {
+                path: '',
+                redirectTo: 'offers'
+            },
+            {
+                path: 'offers',
+                loadChildren: '../pages/offer/offer.module#OfferModule'
+            },
+            {
+                path: 'dialogs',
+                loadChildren: '../pages/messenger/messenger.module#MessengerModule',
+                ...canActivate(redirectUnauthorizedToLogin)
+            },
+            {
+                path: 'profile',
+                loadChildren: '../pages/profile/profile.module#ProfileModule',
+                ...canActivate(redirectUnauthorizedToLogin)
+            }
         ]
     }
 ];
