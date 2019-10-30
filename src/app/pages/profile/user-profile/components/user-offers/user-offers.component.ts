78463dbc534b85ca12c9e7a68bba849452fcc857
kotytik <ed@brightmobile.ru>
2019-10-30 13:35:51 +0400

    fix

--- src/app/pages/profile/user-profile/components/user-offers/user-offers.component.ts
+++ src/app/pages/profile/user-profile/components/user-offers/user-offers.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {OfferService} from '../../../../../services/offer/offer.service';
 import {Offer} from '../../../../../interfaces/model/offer';
 import {IMAGE_SETTINGS} from '../../../../../config/no-image.settings';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-user-offers',
@@ -12,11 +14,38 @@ export class UserOffersComponent implements OnInit {
 
     noImageUrl = IMAGE_SETTINGS.NO_IMAGE;
 
-    constructor(private offerService: OfferService) {
+    constructor(private offerService: OfferService,
+        public alertController: AlertController,
+        private translateService: TranslateService,
+        ) {
     }
 
     offers;
-
+    async presentAlert(offer) {
+        this.translateService.get(['cancel',"yes",'delQuest']).subscribe(async (res: string) => {
+            const alert = await this.alertController.create({
+               
+                message: res["delQuest"],
+                buttons:  [
+                    {
+                      text: res["cancel"],
+                      role: "cancel",
+                    
+                    },
+                    {
+                        text: res["yes"],
+    
+                        handler: () => {
+                          this.remove(offer);
+                        }
+                      }
+                  ]
+              });
+          
+              await alert.present();
+          });
+       
+      }
     ngOnInit() {
         this.offers =   this.offerService.getMy();
     }
